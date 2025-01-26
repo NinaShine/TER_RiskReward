01057f2b812e34fa431f6696bbdfb75cd548c510
arkan <ramy.massal@etu.umontpellier.fr>
2025-01-26 22:11:11 +0100

    Ajout des sliders interactifs sur la page principale

---
+++ frontend/frontend-app/.angular/cache/19.1.4/frontend-app/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-7KWSOJZE.js";
+import "./chunk-5FEBRZLI.js";
+import "./chunk-FUDERPCY.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
--- frontend/frontend-app/src/app/app.component.ts
+++ frontend/frontend-app/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { MatSliderModule } from "@angular/material/slider";
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, MatSliderModule],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'frontend-app';
+  title = "frontend-app";
 }
