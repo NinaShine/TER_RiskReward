b9bfd871cbd9b125612a1fc7560cffa8043d9206
arkan <ramy.massal@etu.umontpellier.fr>
2025-01-31 14:22:59 +0100

    randomroute

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 require("dotenv").config();
 require("./config/db_conn.js");
+
+const app = express();
+const route = require("./routes/Route");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const randomTextRoutes = require("./routes/randomText");
+app.use("/api", randomTextRoutes);
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes/Route"));
-
+app.use("/", route);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- backend/routes/randomText.js
+++ backend/routes/randomText.js
@@ -1,33 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Text = require("../models/textModel");
-const Response = require("../models/responseModel");
-
-// Route pour ajouter un texte
-router.post("/text", async (req, res) => {
-  try {
-    const newText = new Text(req.body);
-    const savedText = await newText.save();
-    res.status(201).json(savedText);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Erreur lors de l'ajout du texte", error: err.message });
-  }
-});
-
-// Route pour récupérer tous les textes
-router.get("/text", async (req, res) => {
-  try {
-    const texts = await Text.find();
-    res.status(200).json(texts);
-  } catch (err) {
-    res.status(500).json({
-      message: "Erreur lors de la récupération des textes",
-      error: err.message,
-    });
-  }
-});
+const Text = require("../models/textModel"); // Assurez-vous que le modèle est correct
 
 // Fonction pour récupérer un texte et une image aléatoire
 async function getRandomText() {
@@ -37,7 +10,7 @@ async function getRandomText() {
   return Text.findOne().skip(randomIndex);
 }
 
-// Route qui retourne un texte et une image aléatoire
+// Définition de la route
 router.get("/random-text", async (req, res) => {
   try {
     const randomText = await getRandomText();
