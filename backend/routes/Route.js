f051a29cc8bf1695f6c5eb942b3d81290312a16a
FranckRvEtu <franck.reveille@etu.umontpellier.fr>
2025-02-05 16:21:39 +0100

    Rajout de la génération du contexte, enregistrement de celui-ci dans les cookies et retransmission sur la route submit. Rajout de la fonction getIndividus() dans le router de la route /init

--- backend/routes/Route.js
+++ backend/routes/Route.js
@@ -29,6 +29,7 @@ router.get("/text", async (req, res) => {
   }
 });
 
+
 // Fonction pour récupérer un texte et une image aléatoire
 async function getRandomText() {
   const count = await Text.countDocuments();
@@ -57,17 +58,72 @@ router.get("/random-text", async (req, res) => {
   }
 });
 
+router.get("/init", async (req,res)=>{
+  //console.log("Session actuelle :", req.session);
+  const scenario = await getRandomText();
+  const individus = await getIndividus();
+  //console.log(scenario);
+  //console.log(individus);
+  req.session.scenario = {
+    textId:scenario.id,
+    text:scenario.content,
+    image:scenario.imageUrl,
+    individuA:individus.a,
+    individuB:individus.b
+  }
+  req.session.save(err=>{
+    if (err){
+      console.error("Erreur de save de la session : ",err);
+      return res.status(500).json({error:"Internal Server Error"});
+    }
+  })
+  res.json(req.session.scenario);
+})
 
 router.post("/submit",async(req,res)=>{ //Cette route devra push les données dans la bdd
                                         //Il faut aussi rajouter plus de contexte à la fonction si on veut éviter d'avoir à faire jongler les infos.
-  console.log("Route appelé");
+  const scenario = req.session.scenario;
+  console.log("Récupération de la session : ",scenario);
+  if (!req.session.scenario) {
+    console.error("Aucune session trouvée !");
+    return res.status(400).json({ error: "Session non trouvée. Avez-vous bien appelé /init avant ?" });
+  }
   try{
     const body = req.body;
     res.json({
       content:body
     });
+    const {sliderValue1, sliderValue2} = req.body;
+    console.log("Récup des slides : ",sliderValue1," - ",sliderValue2);
+    if (!req.session.scenario) {
+      return res.status(400).json({ message: "Session invalide ou expirée." });
+    }
   }catch (error){
     res.status(500).json({message:"Erreur serveur",error});
   }
 });
 module.exports = router;
+
+async function getIndividus(){
+  const tab = [
+    "vieux pas genré",
+    "enfant pas genré",
+    "robot",
+    "homme petite taille",
+    "femme petite taille",
+    "homme grande taille",
+    "femme grande taille",
+  ];
+  const indexa = Math.floor(Math.random()*tab.length);
+  let j = Math.floor(Math.random()*tab.length);
+  while (j==indexa){
+    j = Math.floor(Math.random()*tab.length);
+  }
+  const indexb = j;
+  const a = tab[indexa];
+  const b = tab[indexb];
+  return {
+    a:a,
+    b:b
+  }
+}
