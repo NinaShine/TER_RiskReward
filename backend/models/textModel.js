e6c6b6eaa56255f1116836e5c1da260c159be8da
arkan <ramy.massal@etu.umontpellier.fr>
2025-02-06 13:40:28 +0100

    fix de textModels

--- backend/models/textModel.js
+++ backend/models/textModel.js
@@ -9,9 +9,10 @@ const textSchema = new mongoose.Schema({
     type: String, // URL de l'image
     required: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now, // Date de création par défaut
+  associationType: {
+    type: String,
+    enum: ["risk-reward", "risk-effort", "effort-reward"],
+    required: true,
   },
 });
 
