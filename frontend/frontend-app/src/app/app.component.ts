a3fae27af787662ae5e9ceb0285ba60dfe2478d5
Daniel <danielvargasvila09@hotmail.com>
2025-02-07 22:41:08 +0100

    page accueil fini

--- frontend/frontend-app/src/app/app.component.ts
+++ frontend/frontend-app/src/app/app.component.ts
@@ -4,15 +4,17 @@ import { ImgTextComponent } from "./component/img-text/img-text.component";
 import { SubmitComponent } from "./component/submit/submit.component";
 import { Stickman1Component } from "./component/stickman1/stickman1.component";
 import { Stickman2Component } from "./component/stickman2/stickman2.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { HomePageComponent } from "./component/home-page/home-page.component";
 
 @Component({
   selector: "app-root",
-  imports: [SliderComponent, SubmitComponent, ImgTextComponent, Stickman1Component, Stickman2Component],
+  imports: [SliderComponent, SubmitComponent, ImgTextComponent, Stickman1Component, Stickman2Component, HeaderComponent, HomePageComponent],
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.css",
+  styleUrl: "./app.component.css", 
 })
 export class AppComponent implements OnInit {
-  sliderValue1: number = 5; // Stocke la valeur du slider 1
+  sliderValue1: number = 5; // Stocke la valeur du slider 1!!
   sliderValue2: number = 5; // Stocke la valeur du slider 2
   content: string = "Ceci est un texte"; // Stocke le contenu de l'image et du texte
   imageUrl: string = "https://picsum.photos/200"; // Stocke l'url de l'image
