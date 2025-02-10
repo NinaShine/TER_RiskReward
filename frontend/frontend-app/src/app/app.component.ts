3b8b7ea8c246ebb77b8d16c7cbf10b7a13c68706
Daniel <danielvargasvila09@hotmail.com>
2025-02-10 22:32:46 +0100

    routes entre pages fini

--- frontend/frontend-app/src/app/app.component.ts
+++ frontend/frontend-app/src/app/app.component.ts
@@ -4,18 +4,19 @@ import { ImgTextComponent } from "./component/img-text/img-text.component";
 import { SubmitComponent } from "./component/submit/submit.component";
 import { Stickman1Component } from "./component/stickman1/stickman1.component";
 import { Stickman2Component } from "./component/stickman2/stickman2.component";
-import { HeaderComponent } from "./component/header/header.component";
 import { HomePageComponent } from "./component/home-page/home-page.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: "app-root",
-  imports: [SliderComponent, SubmitComponent, ImgTextComponent, Stickman1Component, Stickman2Component, HeaderComponent, HomePageComponent],
+  standalone: true,
+  imports: [RouterOutlet,SliderComponent, SubmitComponent, ImgTextComponent, Stickman1Component, Stickman2Component, HomePageComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css", 
 })
 export class AppComponent implements OnInit {
-  sliderValue1: number = 5; // Stocke la valeur du slider 1!!
-  sliderValue2: number = 5; // Stocke la valeur du slider 2
+  //sliderValue1: number = 5; // Stocke la valeur du slider 1!!
+  //sliderValue2: number = 5; // Stocke la valeur du slider 2
   content: string = "Ceci est un texte"; // Stocke le contenu de l'image et du texte
   imageUrl: string = "https://picsum.photos/200"; // Stocke l'url de l'image
 
--- frontend/frontend-app/src/app/component/home-page/home-page.component.ts
+++ frontend/frontend-app/src/app/component/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
