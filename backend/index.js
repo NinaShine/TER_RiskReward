24aa8218113750baefcb1deaf941bb71481f0fcd
flavio-liguori <78020810+flavio-liguori@users.noreply.github.com>
2025-02-19 11:33:31 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const route = require("./routes/Route");
 
 app.use(
   cors({
-    origin: "https://choice-quizz.onrender.com/",
+    origin: "https://choicesite.vercel.app/",
     credentials: true,
   })
 );
