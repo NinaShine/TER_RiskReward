4366aa481dbc31b0f15657e620f4f31c014264b7
Daniel <danielvargasvila09@hotmail.com>
2025-03-13 18:07:23 +0100

    ajout commentaire route front

--- frontend/frontend-app/src/app/app.routes.ts
+++ frontend/frontend-app/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { ImgTextComponent } from './component/img-text/img-text.component';
 import { HomePageComponent } from './component/home-page/home-page.component';
 import { ForcesPageComponent } from './component/forces-page/forces-page.component';
 import { FinalPageComponent } from './component/final-page/final-page.component';
+//import { Formulaire } from './comoonent/formulaire/formulaire.component';
 
 export const routes: Routes = [
     { path: '', component : HomePageComponent},
     {path : 'img-text', component : ImgTextComponent},
     {path : 'forces', component : ForcesPageComponent},
     {path : 'pageFinale', component : FinalPageComponent}
-
+    //{path : 'formulaire', component : Formulaire}
 ];
